85b0c8ebd70e8acd0ebfd74f92baa7e2c4450d02
Yongchol Choi <charleschoi87@gmail.com>
2015-08-13 11:23:58 +0000

    save dispatch

--- app/controllers/dispatches_controller.rb
+++ app/controllers/dispatches_controller.rb
@@ -3,4 +3,12 @@ class DispatchesController < ApplicationController
     def index
         @destination = Destination.find(params[:id])
     end
+    
+    def create
+        Dispatch.create(title: params[:title],
+                        diary: params[:diary],
+                        emotion: params[:emotion],
+                        when: params[:date]
+                        )
+    end
 end
