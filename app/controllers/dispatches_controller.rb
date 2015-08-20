66e6f06c3a517dcf77aba5c02dbd525a53302e4c
Yongchol Choi <charleschoi87@gmail.com>
2015-08-20 13:29:49 +0000

    save average point of dispatches

--- app/controllers/dispatches_controller.rb
+++ app/controllers/dispatches_controller.rb
@@ -15,6 +15,18 @@ class DispatchesController < ApplicationController
                         lng: params[:lng],
                         picture: params[:picture]
                         )
+        d = Destination.find(params[:destination_id])
+        total_lat = 0
+        total_lng = 0
+        d.dispatches.each do |i|
+            total_lat += i.lat.to_f
+            total_lng += i.lng.to_f
+        end
+        avg_lat = total_lat / d.dispatches.count
+        avg_lng = total_lng / d.dispatches.count
+        d.lat = avg_lat.to_s
+        d.lng = avg_lng.to_s
+        d.save
         redirect_to :back
     end
     
